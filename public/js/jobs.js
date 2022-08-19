d143d1a2241d4badca69e9b35b344814a6112b45
Ulug'bek <93920465+theulubek@users.noreply.github.com>
2022-08-19 17:35:25 +0500

    fetch jobs and render

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,4 +1,4 @@
-const backendApi = 'http://localhost:3000/api';
+const backendApi = 'http://localhost:5000';
 
 const jobContainer = document.querySelector('.container');
 const filters = document.querySelector('.search');
@@ -15,3 +15,43 @@ sortForm.addEventListener('submit', (e) => {
     const data = Object.fromEntries(formData); 
     console.log(data);
 });
+
+async function getJobs(query) {
+    const response = await fetch(`${backendApi}/jobs`);
+    const data = await response.json();
+    return data;
+}
+
+async function renderJobs() {
+    const jobs = await getJobs();
+    console.log(jobs);
+    jobContainer.innerHTML = jobs.data.map(job => `
+        <div class="result">
+        <h3>
+            <a href="jobs/${job.title}-${job.job_id}">${job.title}</a>
+        </h3>
+
+        <ul class="job_info">
+            <li>
+                <img src="./img/category.png" alt="category" class="iconn">
+                ${job.category}
+            </li>
+            <li>
+                <img src="./img/location.png" alt="location" class="iconn">
+                ${job.location}
+            </li>
+            <li>
+                <img src="./img/clock.png" alt="clock" class="iconn">
+                ${job.createdAt.split('T')[0]}
+            </li>
+            <li>
+                <span class="status">${job.status}</span>
+                <span class="time">${job.type}</span>
+                <span class="money">${job.salary}</span>
+            </li>
+        </ul>
+    </div>
+    `).join('');
+}
+
+renderJobs();
